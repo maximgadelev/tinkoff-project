f172ffb6372cd85c7e22d6ac748857bbfd8be05a
GadelevMaxim <maximgadelev@mail.ru>
2022-03-07 20:35:27 +0300

    Create main fragments and add nav graph

--- app/src/main/java/com/example/tinkoff_project/MainActivity.kt
+++ app/src/main/java/com/example/tinkoff_project/MainActivity.kt
@@ -4,14 +4,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.tinkoff_project.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var controller: NavController
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         controller =
             (supportFragmentManager.findFragmentById(R.id.host_fragment) as NavHostFragment).navController
+        val bottom=findViewById<BottomNavigationView>(R.id.bnv_main)
+        bottom.setupWithNavController(controller)
+
     }
     override fun onNavigateUp(): Boolean {
         return controller.navigateUp()
