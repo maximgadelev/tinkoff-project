dddbc00e8d42d682f32a8ec4ea4de25ae75db96f
MariaDimukhametova <maria_dimukhametova@mail.ru>
2022-03-24 18:41:46 +0300

    rebase master

--- app/src/main/java/ru/itis/tinkoff/project/features/catalog/ui/CatalogFragment.kt
+++ app/src/main/java/ru/itis/tinkoff/project/features/catalog/ui/CatalogFragment.kt
@@ -4,26 +4,31 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import by.kirich1409.viewbindingdelegate.viewBinding
+import com.hannesdorfmann.adapterdelegates4.ListDelegationAdapter
 import ru.itis.tinkoff.project.R
 import ru.itis.tinkoff.project.databinding.CatalogFragmentBinding
-import ru.itis.tinkoff.project.features.catalog.adapter.MainAdapter
+import ru.itis.tinkoff.project.features.catalog.adapter.AdapterDelegates
 import ru.itis.tinkoff.project.features.catalog.repository.CategoryRepository
 
 class CatalogFragment : Fragment(R.layout.catalog_fragment) {
 
     private val viewBinding by viewBinding(CatalogFragmentBinding::bind)
+    private val adapter = ListDelegationAdapter (
+        AdapterDelegates.categoryAdapterDelegate
+    )
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewBinding
         init()
     }
 
     private fun init () {
         with(viewBinding) {
-            val adapter = MainAdapter()
-            adapter.items = CategoryRepository.categories
             rvCatalog.adapter = adapter
+            adapter.apply {
+                items = CategoryRepository.categories
+                notifyDataSetChanged()
+            }
         }
     }
 }
