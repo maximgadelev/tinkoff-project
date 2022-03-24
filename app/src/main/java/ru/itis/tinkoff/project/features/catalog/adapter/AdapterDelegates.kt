e9ced6ad7fcb65f314bbfbe5a3cab6894ca79462
MariaDimukhametova <maria_dimukhametova@mail.ru>
2022-03-24 18:53:23 +0300

    fix syntax errors

--- app/src/main/java/ru/itis/tinkoff/project/features/catalog/adapter/AdapterDelegates.kt
+++ app/src/main/java/ru/itis/tinkoff/project/features/catalog/adapter/AdapterDelegates.kt
@@ -1,12 +1,9 @@
 package ru.itis.tinkoff.project.features.catalog.adapter
 
-import com.hannesdorfmann.adapterdelegates4.ListDelegationAdapter
 import com.hannesdorfmann.adapterdelegates4.dsl.adapterDelegateViewBinding
-import ru.itis.tinkoff.project.databinding.CatalogFragmentBinding
 import ru.itis.tinkoff.project.databinding.ItemCategoryBinding
 import ru.itis.tinkoff.project.features.catalog.model.Category
 import ru.itis.tinkoff.project.features.catalog.model.DisplayableItem
-import ru.itis.tinkoff.project.features.catalog.repository.CategoryRepository
 
 object AdapterDelegates {
 
@@ -25,24 +22,4 @@ object AdapterDelegates {
         }
     }
 
-    /*val catalogAdapterDelegate = adapterDelegateViewBinding<Category, DisplayableItem, CatalogFragmentBinding>(
-        { layoutInflater, root ->
-            CatalogFragmentBinding.inflate(
-                layoutInflater,
-                root,
-                false
-            ).apply {
-                rvCatalog.adapter = categoryAdapter
-            }
-        }
-    ) {     bind {
-                (binding.rvCatalog.adapter as ListDelegationAdapter<*>).apply {
-                    items = CategoryRepository.categories
-                }
-            }
-    }
-
-    private val categoryAdapter = ListDelegationAdapter(
-        categoryAdapterDelegate
-    )*/
 }
