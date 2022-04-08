aa09e3a0fe0542acb7f3b85bcd3ad6e5ae1cf8b2
GadelevMaxim <maximgadelev@mail.ru>
2022-04-08 17:07:38 +0300

    add network module

--- app/src/main/java/ru/itis/tinkoff/project/di/DIContainer.kt
+++ app/src/main/java/ru/itis/tinkoff/project/di/DIContainer.kt
@@ -1,7 +1,11 @@
 package ru.itis.tinkoff.project.di
 
+import okhttp3.OkHttpClient
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import ru.itis.tinkoff.project.BuildConfig
 import ru.itis.tinkoff.project.data.Api
 import ru.itis.tinkoff.project.data.StubApi
 import ru.itis.tinkoff.project.features.common.mapper.EntityMapper
@@ -9,6 +13,7 @@ import ru.itis.tinkoff.project.features.favorites.data.FavoritesRepository
 import ru.itis.tinkoff.project.features.favorites.ui.FavoritesViewModel
 import ru.itis.tinkoff.project.features.main.data.MenuRepository
 import ru.itis.tinkoff.project.features.main.ui.MainViewModel
+import ru.itis.tinkoff.project.network.BaseInterceptor
 
 val appModule = module {
     single<EntityMapper> { EntityMapper() }
@@ -29,4 +34,18 @@ val dataModule = module {
     single<Api> { StubApi() }
     single<MenuRepository> { MenuRepository(api = get()) }
     single<FavoritesRepository> { FavoritesRepository(api = get()) }
+}
+val networkModule = module {
+    factory { BaseInterceptor() }
+    factory<OkHttpClient> { provideOkHttpClient(get()) }
+    single<Retrofit> { provideRetrofit(get()) }
+}
+
+fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+    return Retrofit.Builder().baseUrl(BuildConfig.API_URL).client(okHttpClient)
+        .addConverterFactory(GsonConverterFactory.create()).build()
+}
+
+fun provideOkHttpClient(baseInterceptor: BaseInterceptor): OkHttpClient {
+    return OkHttpClient().newBuilder().addInterceptor(baseInterceptor).build()
 }
