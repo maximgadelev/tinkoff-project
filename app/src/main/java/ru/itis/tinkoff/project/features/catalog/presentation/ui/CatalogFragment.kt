da709d5f9672ccffffd60a0e4ae6d1a3475df277
MariaDimukhametova <maria_dimukhametova@mail.ru>
2022-04-14 14:05:40 +0300

    rebase

--- app/src/main/java/ru/itis/tinkoff/project/features/catalog/presentation/ui/CatalogFragment.kt
+++ app/src/main/java/ru/itis/tinkoff/project/features/catalog/presentation/ui/CatalogFragment.kt
@@ -3,44 +3,38 @@ package ru.itis.tinkoff.project.features.catalog.presentation.ui
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
 import by.kirich1409.viewbindingdelegate.viewBinding
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
 import ru.haroncode.aquarius.core.RenderAdapterBuilder
 import ru.haroncode.aquarius.core.base.strategies.DifferStrategies
 import ru.itis.tinkoff.project.R
-import ru.itis.tinkoff.project.features.catalog.presentation.ui.renderer.CategoryRenderer
-import ru.itis.tinkoff.project.features.catalog.presentation.ui.viewModel.CategoryViewModel
-import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.itis.tinkoff.project.databinding.CatalogFragmentBinding
-import ru.itis.tinkoff.project.features.catalog.presentation.ui.renderer.CatalogRenderer
-import ru.itis.tinkoff.project.features.catalog.utils.CatalogListItem
+import ru.itis.tinkoff.project.entity.Category
+import ru.itis.tinkoff.project.features.catalog.presentation.ui.renderer.CategoryListRenderer
+import ru.itis.tinkoff.project.features.catalog.presentation.ui.utils.CategoryItemProvider
+import ru.itis.tinkoff.project.features.catalog.presentation.ui.viewModel.CategoryViewModel
 
 class CatalogFragment : Fragment(R.layout.catalog_fragment) {
 
     private val viewBinding by viewBinding(CatalogFragmentBinding::bind)
-    private val viewModel: CategoryViewModel by viewModel()
-    private val itemAdapter by lazy {
-        RenderAdapterBuilder<CatalogListItem>()
-            .renderer(CatalogListItem.CategoryListItem::class, CategoryRenderer())
-            .renderer(CatalogListItem::class, CatalogRenderer())
+    //private val viewModel: CategoryViewModel by viewModel()
+    private val categoryItemProvider = CategoryItemProvider()
+
+    private val adapter by lazy {
+        RenderAdapterBuilder<Category>()
+            .renderer(Category::class, CategoryListRenderer())
             .build(DifferStrategies.withDiffUtilComparable())
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initList()
-        viewModel.item.onEach {
-            itemAdapter.differ.submitList(it)
-        }
-            .launchIn(lifecycleScope)
+        init()
     }
 
-    private fun initList() {
+    private fun init () {
         with(viewBinding) {
-            rvCatalog.adapter = itemAdapter
+            rvCatalog.adapter = adapter
         }
+        adapter.differ.submitList(categoryItemProvider.getItemList(context))
     }
 }
 
