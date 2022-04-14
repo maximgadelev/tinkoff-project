4047ca706473f1cb7e1ae6308a60aca5fd8094e6
MariaDimukhametova <maria_dimukhametova@mail.ru>
2022-04-14 14:05:40 +0300

    add CategoryRenderer

--- app/src/main/java/ru/itis/tinkoff/project/entity/Category.kt
+++ app/src/main/java/ru/itis/tinkoff/project/entity/Category.kt
@@ -1,6 +1,13 @@
 package ru.itis.tinkoff.project.entity
 
-data class Category(
-    val name: String
-)
+import ru.haroncode.aquarius.core.diffutil.ComparableItem
+import ru.itis.tinkoff.project.features.catalog.utils.DisplayableItem
+
+data class Category (
+    val id: Long,
+    val photo: Int,
+    val name: String,
+) : DisplayableItem, ComparableItem
+
+
 
