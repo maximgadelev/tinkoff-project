e0b80bb992d77d00f2977d4332b585059e2e54a2
MariaDimukhametova <maria_dimukhametova@mail.ru>
2022-04-14 14:05:40 +0300

    rebase

---
+++ app/src/main/java/ru/itis/tinkoff/project/features/catalog/presentation/ui/renderer/CategoryListRenderer.kt
@@ -0,0 +1,35 @@
+package ru.itis.tinkoff.project.features.catalog.presentation.ui.renderer
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import kotlinx.android.synthetic.main.item_category_catalog.view.*
+import ru.haroncode.aquarius.core.clicker.ClickableRenderer
+import ru.haroncode.aquarius.core.renderer.ItemBaseRenderer
+import ru.itis.tinkoff.project.R
+
+class CategoryListRenderer<Item> : ItemBaseRenderer<Item, CategoryListRenderer.RenderContract>(), ClickableRenderer {
+
+    interface RenderContract {
+        val title: String
+        val icon: Int
+    }
+
+    override fun bindClickListener(
+        viewHolder: RecyclerView.ViewHolder,
+        listener: (RecyclerView.ViewHolder, View) -> Unit
+    ) {
+        viewHolder.itemView.setOnClickListener {
+            listener(viewHolder, it)
+        }
+    }
+
+    override fun onBindView(viewHolder: BaseViewHolder, item: RenderContract) {
+        with(viewHolder) {
+            itemView.tv_category_name.text = item.title
+            itemView.iv_icon_category.load(item.icon)
+        }
+    }
+
+    override val layoutRes: Int = R.layout.item_category_catalog
+}
