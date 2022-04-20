b645fab7c4efe96c9f0a84eff6118774ead1be9c
GadelevMaxim <maximgadelev@mail.ru>
2022-04-21 00:56:26 +0300

    try fix bug

--- app/src/main/java/ru/itis/tinkoff/project/features/common/ErrorEvent.kt
+++ app/src/main/java/ru/itis/tinkoff/project/features/common/ErrorEvent.kt
@@ -1,3 +1,5 @@
 package ru.itis.tinkoff.project.features.common
 
-object ErrorEvent {}
+object ErrorEvent {
+    var title = "Error" // на меня buildAction ругается в build,если ErrorEvent пустой
+}
