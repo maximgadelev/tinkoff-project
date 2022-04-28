29eacf4f11889b17e7b296fcdfc5d858f5518157
MariaDimukhametova <maria_dimukhametova@mail.ru>
2022-04-28 11:34:48 +0300

    fix

--- app/src/main/java/ru/itis/tinkoff/project/entity/Category.kt
+++ app/src/main/java/ru/itis/tinkoff/project/entity/Category.kt
@@ -1,13 +1,12 @@
 package ru.itis.tinkoff.project.entity
 
 import ru.haroncode.aquarius.core.diffutil.ComparableItem
-import ru.itis.tinkoff.project.features.catalog.utils.DisplayableItem
 
 data class Category (
     val id: Long,
     val photo: Int,
     val name: String,
-) : DisplayableItem, ComparableItem
+) : ComparableItem
 
 
 
