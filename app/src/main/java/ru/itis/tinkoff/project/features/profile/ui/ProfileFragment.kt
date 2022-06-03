aad7f797770a4549f13506cf7f598ee3b6677e07
Elina Kadyrova <kad_elina@mail.ru>
2022-06-03 20:35:44 +0300

    Add photo downloading without result api

--- app/src/main/java/ru/itis/tinkoff/project/features/profile/ui/ProfileFragment.kt
+++ app/src/main/java/ru/itis/tinkoff/project/features/profile/ui/ProfileFragment.kt
@@ -1,11 +1,22 @@
 package ru.itis.tinkoff.project.features.profile.ui
 
+import android.Manifest
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
+import coil.load
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -28,6 +39,9 @@ class ProfileFragment : Fragment(R.layout.profile_fragment) {
             .renderer(ProfileOptionItem::class, ProfileOptionListRenderer())
             .build(DifferStrategies.withDiffUtilComparable())
     }
+    private lateinit var alertDialog: AlertDialog
+    private val REQUEST_CODE_LOAD = 1001
+    private val REQUEST_CODE_TAKE_PHOTO = 1002
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -36,6 +50,9 @@ class ProfileFragment : Fragment(R.layout.profile_fragment) {
         initSwipeToRefreshLayout()
         viewBinding.outBtn.setOnClickListener {
             findNavController().navigate(R.id.action_profileFragment_to_authorizationFragment)
+        }
+        viewBinding.ivAvatar.setOnClickListener {
+            showAlert()
         }
     }
 
@@ -74,5 +91,126 @@ class ProfileFragment : Fragment(R.layout.profile_fragment) {
                 R.color.deep_orange
             )
         }
+    }
+
+    private fun showAlert() {
+        val builder = AlertDialog.Builder(context)
+        val optionsForLoading = arrayOf<CharSequence>("Сделать фото","Загрузить с устройства")
+        builder
+            .setTitle("Изменить аватар профиля")
+            .setItems(optionsForLoading){_, which->
+                if (which == 0) {
+                    addAvatarByTakingPhoto()
+                }
+                if (which == 1) {
+                    addAvatarByLoading()
+                }
+            }
+            .setNegativeButton("Отмена"){dialog, _->
+                dialog.cancel()
+            }
+            .show()
+        alertDialog= builder.create()
+    }
+
+    private fun addAvatarByLoading() {
+        if (checkGalleryPermissions() == true) {
+            chooseImageGallery()
+        } else {
+            requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),REQUEST_CODE_LOAD)
+        }
+    }
+
+    private fun addAvatarByTakingPhoto() {
+        if (checkCameraPermissions() == true) {
+            takeCameraPhoto()
+        } else {
+            requestPermissions(arrayOf(Manifest.permission.CAMERA),REQUEST_CODE_TAKE_PHOTO)
+        }
+    }
+
+    private fun checkGalleryPermissions(): Boolean? {
+        activity?.apply{
+            return ContextCompat.checkSelfPermission(
+                applicationContext,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED&& ContextCompat.checkSelfPermission(
+                applicationContext,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED
+        }
+        return false
+    }
+
+    private fun checkCameraPermissions(): Boolean? {
+        activity?.apply{
+            return ContextCompat.checkSelfPermission(
+                applicationContext,
+                Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED&& ContextCompat.checkSelfPermission(
+                applicationContext,
+                Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED
+        }
+        return false
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            REQUEST_CODE_LOAD->
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    chooseImageGallery()
+                } else {
+                    Toast.makeText(context,"Доступ к галерее запрещен", Toast.LENGTH_SHORT).show()
+                }
+            REQUEST_CODE_TAKE_PHOTO->
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    takeCameraPhoto()
+                } else {
+                    Toast.makeText(context,"Доступ к камере запрещен", Toast.LENGTH_SHORT).show()
+                }
+        }
+    }
+
+    private fun chooseImageGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type="image/*"
+        startActivityForResult(intent,REQUEST_CODE_LOAD)
+    }
+
+    private fun takeCameraPhoto() {
+        val callCameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(callCameraIntent,REQUEST_CODE_TAKE_PHOTO)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        when (requestCode) {
+            REQUEST_CODE_LOAD-> {
+                if (resultCode == Activity.RESULT_OK) {
+                    data?.data?.let{loadWithCoil(it)}
+                }
+            }
+            REQUEST_CODE_TAKE_PHOTO->
+                if (resultCode == Activity.RESULT_OK) {
+                    with(viewBinding){
+                        ivAvatar.setImageBitmap(data?.extras?.get("data") as Bitmap)
+                    }
+                }
+        }
+    }
+
+    private fun loadWithCoil(imageRoute: Uri) {
+        with(viewBinding){
+            ivAvatar.load(imageRoute){
+                crossfade(true)
+                placeholder(R.drawable.user_photo_default)
+                error(R.drawable.user_photo_default)
+            }
+        }
+        Toast.makeText(context,"Фото профиля успешно изменено", Toast.LENGTH_SHORT).show()
     }
 }
