9e79c4e7fc0509a82305d90f88d0cefe3c0a21c4
Elina Kadyrova <71032394+Elina-Kadyrova@users.noreply.github.com>
2022-06-05 15:52:00 +0300

    Task/profile photo (#28)
    * Add photo downloading without result api
    
    * Attempts to add result api
    
    * Add result api
    
    * Add crop dependency
    
    * Fix detekt
    
    * Fix detekt again
    
    * Delete lateinit dialog)

--- app/src/main/java/ru/itis/tinkoff/project/features/profile/ui/ProfileFragment.kt
+++ app/src/main/java/ru/itis/tinkoff/project/features/profile/ui/ProfileFragment.kt
@@ -1,11 +1,19 @@
 package ru.itis.tinkoff.project.features.profile.ui
 
+import android.Manifest
+import android.app.AlertDialog
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.launch
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
+import coil.load
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -28,6 +36,36 @@ class ProfileFragment : Fragment(R.layout.profile_fragment) {
             .renderer(ProfileOptionItem::class, ProfileOptionListRenderer())
             .build(DifferStrategies.withDiffUtilComparable())
     }
+    private val galleryLoadingPermission = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+        when {
+            it -> galleryGetContent.launch(getString(R.string.image_gallery_uri))
+            else -> Toast.makeText(context, getString(R.string.access_to_gallery_forbidden), Toast.LENGTH_SHORT).show()
+        }
+    }
+    private val cameraLoadingPermission = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+        when {
+            it -> cameraTakePicture.launch()
+            else -> Toast.makeText(context, getString(R.string.access_to_camera_forbidden), Toast.LENGTH_SHORT).show()
+        }
+    }
+    private val cameraTakePicture = registerForActivityResult(ActivityResultContracts.TakePicturePreview()) {
+            bitmap: Bitmap? -> bitmap?.let {
+                viewBinding.ivAvatar.setImageBitmap(bitmap)
+                Toast.makeText(
+                    context,
+                    getString(R.string.profile_photo_successfully_loaded),
+                    Toast.LENGTH_SHORT).show()
+            }
+    }
+    private val galleryGetContent = registerForActivityResult(ActivityResultContracts.GetContent()) {
+            uri: Uri? -> uri?.let {
+                loadWithCoil(it)
+                Toast.makeText(
+                    context,
+                    getString(R.string.profile_photo_successfully_loaded),
+                    Toast.LENGTH_SHORT).show()
+            }
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -36,6 +74,9 @@ class ProfileFragment : Fragment(R.layout.profile_fragment) {
         initSwipeToRefreshLayout()
         viewBinding.outBtn.setOnClickListener {
             findNavController().navigate(R.id.action_profileFragment_to_authorizationFragment)
+        }
+        viewBinding.ivAvatar.setOnClickListener {
+            showAlert()
         }
     }
 
@@ -57,10 +98,11 @@ class ProfileFragment : Fragment(R.layout.profile_fragment) {
         with(viewBinding) {
             tvName.text = profile.firstName
             tvSurname.text = profile.lastName
-            /*val avatarBitmap = BitmapFactory.decodeFile(profile.profileImg)
-            avatarBitmap?.also{
-                ivAvatar.setImageBitmap(it)
-            }*/
+            ivAvatar.load(profile.profileImg) {
+                crossfade(true)
+                placeholder(R.drawable.user_photo_default)
+                error(R.drawable.user_photo_default)
+            }
         }
     }
 
@@ -73,6 +115,47 @@ class ProfileFragment : Fragment(R.layout.profile_fragment) {
             swipeRefreshLayout.setColorSchemeResources(
                 R.color.deep_orange
             )
+        }
+    }
+
+    private fun showAlert() {
+        val builder = AlertDialog.Builder(context)
+        val optionsForLoading = arrayOf<CharSequence>(
+            getString(R.string.take_picture),
+            getString(R.string.load_from_device)
+        )
+        builder
+            .setTitle(getString(R.string.change_avatar))
+            .setItems(optionsForLoading) { _, which ->
+                if (which == 0) {
+                    addAvatarByTakingPhoto()
+                }
+                if (which == 1) {
+                    addAvatarByLoading()
+                }
+            }
+            .setNegativeButton(getString(R.string.cancel)) { dialog, _ ->
+                dialog.cancel()
+            }
+            .show()
+        builder.create()
+    }
+
+    private fun addAvatarByLoading() {
+       galleryLoadingPermission.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+    }
+
+    private fun addAvatarByTakingPhoto() {
+        cameraLoadingPermission.launch(Manifest.permission.CAMERA)
+    }
+
+    private fun loadWithCoil(imageRoute: Uri) {
+        with(viewBinding) {
+            ivAvatar.load(imageRoute) {
+                crossfade(true)
+                placeholder(R.drawable.user_photo_default)
+                error(R.drawable.user_photo_default)
+            }
         }
     }
 }
